1cc49d6af579267d5bfe8e7e2eca501c052cdb43
miro <miro.glagolic@gmail.com>
2019-09-18 20:16:05 +0200

    formik to login form

--- src/screens/login-screen/LoginScreen.js
+++ src/screens/login-screen/LoginScreen.js
@@ -4,6 +4,7 @@ import React, {lazy} from 'react';
 import ButtonVariants from '../../_shared components/Button/ButtonVariants';
 import ButtonSizes from '../../_shared components/Button/ButtonSizes';
 import { GoogleLogin } from 'react-google-login';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 const Button = lazy(() => import('../../_shared components/Button/Button'));
 
@@ -40,6 +41,39 @@ function fLogin() {
             <div className="container">
                 {/* <Button variant={ButtonVariants.primary} size={ButtonSizes.large}>Login</Button>
                 <Button variant={ButtonVariants.primary} size={ButtonSizes.large}>Google</Button> */}
+             <Formik
+                initialValues={{ email: '', password: '' }}
+                validate={values => {
+                  let errors = {};
+                  if (!values.email) {
+                    errors.email = 'Required';
+                  } else if (
+                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                  ) {
+                    errors.email = 'Invalid email address';
+                  }
+                  return errors;
+                }}
+                onSubmit={(values, { setSubmitting }) => {
+                  setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    setSubmitting(false);
+                  }, 400);
+                }}
+              >
+                {({ isSubmitting }) => (
+                  <Form>
+                    <Field type="email" name="email" />
+                    <ErrorMessage name="email" component="div" />
+                    <Field type="password" name="password" />
+                    <ErrorMessage name="password" component="div" />
+                    <button type="submit" disabled={isSubmitting}>
+                      Submit
+                    </button>
+                  </Form>
+                )}
+              </Formik>
+
                 <div>
                     <GoogleLogin
                         clientId={config.GOOGLE_CLIENT_ID}
