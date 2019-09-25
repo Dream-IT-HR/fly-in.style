cbfcc9da4cc65118de77bb132a5b7edb130c9df8
Miro Glagolić <m.glagolic@emakina.hr>
2019-09-25 09:18:41 +0200

    modal form

---
+++ src/_custom hooks/useModal.js
@@ -0,0 +1,16 @@
+import { useState } from 'reactn';
+
+const useModal = () => {
+  const [isShowing, setIsShowing] = useState(false);
+
+  function toggle() {
+    setIsShowing(!isShowing);
+  }
+
+  return {
+    isShowing,
+    toggle,
+  }
+};
+
+export default useModal;
